68d7ceb68c8c7a8bc33167708947933eaf41a3f5
yash_pakala <yashashvinipakala@gmail.com>
2017-07-15 16:59:30 -0500

    package issues

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os.path
 import sys
 import json
 import datetime
-from flask.ext.cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 cors = CORS(app, resources={r"/": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 # try:
